c617aeba0bf13e6783271fdc44402c2ae56e9fb8
Greg Jurzak <greg.jurzak@mobile-5.com>
2020-02-13 14:07:30 +0100

    Fix

--- Package.swift
+++ Package.swift
@@ -3,23 +3,13 @@
 
 import PackageDescription
 
-// TODO: resolve problem with sources shared to all targets
-
 let package = Package(
     name: "KeyedCodable",
-//    platforms: [.macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v2)],
-    platforms: [.iOS(.v8)],
+    platforms: [.macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v2)],
     products: [
-        .library(
-            name: "KeyedCodable",
-//            targets: ["KeyedCodable-iOS", "KeyedCodable-watchOS", "KeyedCodable-tvOS", "KeyedCodable-macOS"]
-            targets: ["KeyedCodable"]
-        ),
+        .library(name: "KeyedCodable", targets: ["KeyedCodable"]),
     ],
     targets: [
-        .target(name: "KeyedCodable", path: "KeyedCodable/Sources"),
-//        .target(name: "KeyedCodable-watchOS", path: "KeyedCodable/Sources"),
-//        .target(name: "KeyedCodable-tvOS", path: "KeyedCodable/Sources"),
-//        .target(name: "KeyedCodable-macOS", path: "KeyedCodable/Sources")
+        .target(name: "KeyedCodable", path: "KeyedCodable/Sources")
     ]
 )
